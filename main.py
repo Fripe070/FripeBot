e91158d607418b9ffd1db0618777c5b58b211a9f
Fripe070 <friptegaming@gmail.com>
2021-04-06 11:05:00 +0200

    Added the "Voice" cog for voice chat related commands Also moved the existing voice commands into that cog

--- main.py
+++ main.py
@@ -134,29 +134,6 @@ async def mailfripe(ctx, *, arg):
         await ctx.send("Messaged Fripe!")
         await bot.get_channel(823989070845444106).send(f'{ctx.author.mention}\n- {arg}')
 
-
-# VC COMMANDS -----------------------------------------------------------------------------------
-# Joining a VC:
-@bot.command(name="VCjoin", help="Joins the user's VC")
-async def vcjoin(ctx):
-    if ctx.author.voice is None:
-        # Exiting if the user is not in a voice channel
-        return await ctx.send('You need to be in a voice channel to use this command!')
-    else:
-        channel = ctx.author.voice.channel  # Get the sender's voice channel
-        await channel.connect()  # Join the channel
-
-
-# Leaving a VC:
-@bot.command(name="VCleave", help="Leaves the VC", pass_context=True)
-async def vcleave(ctx):
-    if ctx.author.voice is None:
-        # Exiting if the user is not in a voice channel
-        return await ctx.send('You need to be in a voice channel to use this command!')
-    else:
-        server = ctx.message.guild.voice_client  # Get the server of the sender, specific VC doesn't matter.
-        await server.disconnect()  # Leave the VC
-
 # RUN THE BOT -----------------------------------------------------------------------------------
 load_dotenv()
 bot.run(os.getenv('TOKEN'))
