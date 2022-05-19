1f7caeff5aa8215479a2168527c26aa3a5f0cb9d
Fripe070 <Fripe070@users.noreply.github.com>
2022-05-19 14:16:08 +0000

    Commit formating changes made by linter

--- cogs/info.py
+++ cogs/info.py
@@ -202,7 +202,7 @@ Likes/Dislikes: {r['thumbs_up']}/{r['thumbs_down']}
             embed.add_field(
                 name=wiki["title"],
                 value=f"[link]({url}?curid={wiki['pageid']})\n"
-                      + re.sub(r"{\\displaystyle (.*?)}", r"\1", re.sub(r"<.*?>(.*?)<.*?>", r"\1", wiki["snippet"])),
+                + re.sub(r"{\\displaystyle (.*?)}", r"\1", re.sub(r"<.*?>(.*?)<.*?>", r"\1", wiki["snippet"])),
                 inline=False,
             )
         await ctx.reply(embed=embed)
