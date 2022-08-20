e548ced568ec774e6e3a26021dd628e6f80250c2
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2022-08-20 20:42:19 +0200

    Move help command to the meta cog

--- cogs/meta.py
+++ cogs/meta.py
@@ -5,9 +5,38 @@ import discord
 from discord.ext import commands
 
 
+class Help(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return command.qualified_name
+
+    async def send_command_help(self, command):
+        embed = discord.Embed(title=f"Info about: {self.get_command_signature(command)}")
+        if command.help is not None:
+            embed.description(value=command.help)
+        else:
+            embed.description(value="This command doesnt have any further info.")
+        if alias := command.aliases:
+            embed.add_field(name="Aliases", value=", ".join(alias), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", colour=discord.Color.blue())
+        for cog, bot_commands in mapping.items():
+            filtered = await self.filter_commands(bot_commands, sort=True)
+            if command_signatures := [self.get_command_signature(c) for c in filtered]:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(name=cog_name, value=", ".join(command_signatures), inline=False)
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
 class Meta(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.help_command = Help()
+        bot.help_command.cog = self
 
     @commands.command(aliases=["source", "git"], help="Links my GitHub profile")
     async def github(self, ctx: commands.Context, user: discord.User = None):
