1fe87e0e7dcd7da0c975443b0fc6b25073176ea4
XDXD-XDXD <46890315+XDXD-XDXD@users.noreply.github.com>
2022-08-26 20:32:18 +0530

    grammar moment

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -40,7 +40,7 @@ class Listeners(commands.Cog):
             await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
             await msg.delete()
 
-        prefixes = config["prefixes"]  # doesnt when i put this directly inside the regex for some reason
+        prefixes = config["prefixes"]  # doesn't work when i put this directly inside the regex for some reason
         if colours := re.findall(rf"(?:{'|'.join(prefixes)})#([\dA-Fa-f]{{6}})", message.content, flags=re.IGNORECASE):
             for colour in colours:
                 url = f"https://www.colorhexa.com/{colour}.png"
