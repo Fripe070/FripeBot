c173c761d14a1439dfce687832be3e5b4ab0967f
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2022-12-10 14:34:37 +0100

    STOP SCREAMING AT ME

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -309,6 +309,7 @@ First went public: <t:1242554400:D> (<t:1242554400:R>)
         ]
         issues = []
         for project in projects:
+            # noinspection RegExpUnnecessaryNonCapturingGroup
             issues += re.findall(rf"(?:{'|'.join(config['prefixes'])})({project}-\d+)", message.content)
         for issue in issues:
             r = requests.get(f"https://bugs.mojang.com/rest/api/latest/issue/{issue}").json()
